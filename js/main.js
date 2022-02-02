d5420427c12b3d6a053e63b8544ed15de54f8af6
Kenta Miura <mkenta0717@gmail.com>
2022-02-02 14:34:51 +0900

    test

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ function fadeAnime(){
 
   // ふわっ
   $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-10;//要素より、50px上の
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll > elemPos - windowHeight ){
